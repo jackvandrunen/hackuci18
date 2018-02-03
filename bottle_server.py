a3b384ca10a89e6090cc509469ac3f6e0fd70815
Won Jin Choi <wjchoi3@uci.edu>
2018-02-03 09:50:04 -0800

    Addition of '/lookup/'

--- bottle_server.py
+++ bottle_server.py
@@ -11,4 +11,10 @@ def search_results():
     return yelp_api.get_search_json_data(search_terms, location)
 
 
+@bottle.route('/lookup/')
+def biz_lookup():
+    business_id = bottle.request.query.id
+    return yelp_api.get_business_json_data(business_id)
+
+
 bottle.run(host='localhost', port=8080, debug=True)    # TURN DEBUG TO FALSE FOR LIVE VERSION
