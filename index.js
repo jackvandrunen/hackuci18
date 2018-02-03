91cf0947c167eb89bed2eb815e72a32cf625a14b
Jake <jesmith24@comcast.net>
2018-02-03 03:44:02 -0800

    node commit but doesnt work

---
+++ index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// class HelloWorldLetMeSleep extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         Hello {this.props.name}
+//       </div>
+//     );
+//   }
+// }
+
+// ReactDOM.render(
+//   <HelloWorldLetMeSleep name = "BigPapa" />,
+//   document.getElementById('BigPapa')
+// );
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
+registerServiceWorker();
