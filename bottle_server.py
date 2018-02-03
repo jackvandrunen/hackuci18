850f2c9119d6ab9115cb9e75172107834e20ab2a
Jake <jesmith24@comcast.net>
2018-02-03 11:48:54 -0800

    same origin blah blah blah

--- bottle_server.py
+++ bottle_server.py
@@ -1,14 +1,29 @@
-# Bottle server for searches and lookups on Yelp API
+# # Bottle server for searches and lookups on Yelp API
+
+# import bottle
+# import yelp_api
+
+# @bottle.route('/search/')
+# def search_results():
+#     search_terms = bottle.request.query.terms       # Reads query "terms" and "location"
+#     location     = bottle.request.query.location
+#     return yelp_api.get_search_json_data(search_terms, location)
+
+
+# bottle.run(host='localhost', port=8080, debug=True)    # TURN DEBUG TO FALSE FOR LIVE VERSION
 
 import bottle
 import yelp_api
 
+@bottle.hook('after_request')
+def enableCORSAfterRequestHook():
+    print ('After request hook.')
+    bottle.response.headers['Access-Control-Allow-Origin'] = '*'
 
-@bottle.route('/search/')
+@bottle.route('/search/', method='GET')
 def search_results():
     search_terms = bottle.request.query.terms       # Reads query "terms" and "location"
     location     = bottle.request.query.location
     return yelp_api.get_search_json_data(search_terms, location)
 
-
-bottle.run(host='localhost', port=8080, debug=True)    # TURN DEBUG TO FALSE FOR LIVE VERSION
+bottle.run(host='localhost', port=8080, debug=True)
--- yelp-help/src/App.js
+++ yelp-help/src/App.js
@@ -1,7 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Search from  './components/Search'
+import request from 'async-request'
 // import {BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
+
 
 class App extends Component {
   constructor() {
@@ -13,6 +15,52 @@ class App extends Component {
         loading: false
       }
   }
+
+  updateSearchTerm = (searchTerm) => {
+    console.log(searchTerm)
+    if (searchTerm.length === 0) {
+        return null
+    } else {
+        this.getSearchData(searchTerm).then((data) => {
+            this.setState({
+                searched: true,
+                loading: false,
+                results: data
+            })
+        })
+    }
+}
+
+getSearchData = async (searchTerm) => {
+  let response
+  try {
+      response = await request('http://localhost:8080/search/',{
+        method: 'GET',
+        data: {
+          terms: 'pizza', location: 'Irvine'
+        }
+      });
+    //   var request = require("request");
+
+    //   var options = { method: 'GET',
+    //     url: 'http://localhost:8080/search/',
+    //     qs: { terms: 'pizza', location: 'irvine' }
+    //   };
+
+    // request(options, function (error, response, body) {
+    //   if (error) throw new Error(error);
+    // });
+  } catch (e) {
+      console.log(e)
+  }
+  let data
+  try {
+      data = await response.json()
+  } catch (e) {
+      console.log(e)
+  }
+  return data
+}
 
   render() {
     return (
