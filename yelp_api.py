55ffd067f8301fd56140919dc3a6312f2a0e806f
Won Jin Choi <wjchoi3@uci.edu>
2018-02-03 15:05:54 -0800

    Added YelpAPIException class and implemented

--- yelp_api.py
+++ yelp_api.py
@@ -3,6 +3,7 @@
 import requests
 from bs4 import BeautifulSoup
 import json
+import yelp_api_exception
 
 API_HOST = "https://api.yelp.com"
 API_KEY = "Y7tqjnvosHUlt_CzHiXINdMrzGJ5pAYAkpyNyFf-CVbOptscEN-wEYaqBeCiCcuO80FiRiC0UF71wMIA_ad1l4f7yRJ-WhWm9jzENIyl7ZKf7BqioZfQT2RBNVh1WnYx"
@@ -44,45 +45,57 @@ def get_business_json_data(business_id: str) -> dict:
     '''
     Returns JSON data of a certain business.
     '''
-    response = requests.request('GET', format_business_url(business_id), headers=HEADERS)
-    return response.json()
+    try:
+        response = requests.request('GET', format_business_url(business_id), headers=HEADERS)
+        return response.json()
+    except:
+        raise yelp_api_exception.YelpAPIException()
 
 
 def get_search_json_data(terms: str, location: str) -> dict:
     '''
     Returns JSON data with search terms and location.
     '''
-    search_terms = {
-        'term': terms.replace(' ', '+'),
-        'location': location.replace(' ', '+'),
-        'limit': SEARCH_LIMIT
-    }
-    response = requests.request('GET', format_search_url(), headers=HEADERS, params=search_terms)
-    return response.json()
+    try:
+        search_terms = {
+            'term': terms.replace(' ', '+'),
+            'location': location.replace(' ', '+'),
+            'limit': SEARCH_LIMIT
+        }
+        response = requests.request('GET', format_search_url(), headers=HEADERS, params=search_terms)
+        return response.json()
+    except:
+        raise yelp_api_exception.YelpAPIException()
 
 
 def get_reviews_json_data(business_id: str) -> dict:    # ONLY ABLE TO RETRIEVE THREE REVIEWS SORTED BY YELP'S ALGORITHM
     '''
     Returns JSON data of only THREE reviews based on a business ID.
     '''
-    response = requests.request('GET', format_reviews_url(business_id), headers=HEADERS)
-    return response.json()
+    try:
+        response = requests.request('GET', format_reviews_url(business_id), headers=HEADERS)
+        return response.json()
+    except:
+        raise yelp_api_exception.YelpAPIException()
 
 
 def get_all_reviews_json_data(business_id: str) -> dict:    # USES BEAUTIFULSOUP TO RETRIEVE JSON FROM BUSINESS YELP SITE HTML
     '''
     Returns JSON data of all reviews from a business's Yelp website HTML using BeautifulSoup.
     '''
-    html_response = requests.get(get_business_yelp_url(business_id)).text
-    soup = BeautifulSoup(html_response, "lxml")
-    data = str(soup.findAll('script', type="application/ld+json")[0].string)    # Retrieves text between specified tag
-    data = data.strip()     # Gets rid of white space on ends of string
-    return json.loads(data)
+    try:
+        html_response = requests.get(get_business_yelp_url(business_id)).text
+        soup = BeautifulSoup(html_response, "lxml")
+        data = str(soup.findAll('script', type="application/ld+json")[0].string)    # Retrieves text between specified tag
+        data = data.strip()     # Gets rid of white space on ends of string
+        return json.loads(data)
+    except:
+        raise yelp_api_exception.YelpAPIException()
 
 
 # TESTING PURPOSES ONLY
-#if __name__ == "__main__":
-    # print(get_business_json_data('pizza-hut-irvine-3'))
-    # print(get_reviews_json_data('pizza-hut-irvine-3'))
-    # print(get_search_json_data("pizza hut", "Irvine, CA"))
-    # print(get_all_reviews_json_data('pizza-hut-irvine-3'))
+# if __name__ == "__main__":
+#     print(get_business_json_data('pizza-hut-irvine-3'))
+#     print(get_reviews_json_data('pizza-hut-irvine-3'))
+#     print(get_search_json_data("pizza hut", "Irvine, CA"))
+#     print(get_all_reviews_json_data('pizza-hut-irvine-3'))
---
+++ yelp_api_exception.py
@@ -0,0 +1,4 @@
+# Exception class for Yelp's API
+
+class YelpAPIException(Exception):
+    pass
