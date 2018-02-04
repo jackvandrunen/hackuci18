d1dada015da6c7261c7206e1dc7d076146e41695
Jake <jesmith24@comcast.net>
2018-02-04 07:39:25 -0800

    test of array indexing

--- yelp-help/src/components/DetailsPane.js
+++ yelp-help/src/components/DetailsPane.js
@@ -14,56 +14,18 @@ const DetailsPane = (props) => {
       request(options, function (error, response, body) {
         if (error) throw new Error(error);
         var json = JSON.parse(body)
-        console.log(json)
-      });
-    } catch (e) {
-        console.log(e)
-    }
-    // try {
-    //     var options = { method: 'GET',
-    //       url: 'http://localhost:8080/lookup/',
-    //       qs: {id : my_id}
-    //     };
-  
-    //   request(options, function (error, response, body) {
-    //     if (error) throw new Error(error);
-    //     //Begin request to Jack using raw body
-    //     try {
-    //         var options = { method: 'GET',
-    //           url: 'http://localhost:8181/menu/',
-    //           qs: {reviews : body},
-    //           mode: 'no-cors'
-    //         };
-      
-    //       request(options, function (error, response, body) {
-    //         if (error) throw new Error(error);
-    //         var jack_json = JSON.parse(body)
-    //         console.log(jack_json)
-    //         //end request to jack
-            
-    //       });
-    //     } catch (e) {
-    //         console.log(e)
-    //     }
-    //   });
-    // } catch (e) {
-    //     console.log(e)
-    // }
-
-    if (props.match.params.place !== '') {
+        var food_info = json[1]
         return (
             <div className='details-pane-container'>
-                 <div className="details-pane">
-                    <h2>{props.match.params.place}</h2>
+                    <div className="details-pane">
+                    <h2>food_info[0][0]</h2>
+                    <h2>food_info[0][1]</h2>
                 </div> 
             </div>
         )
-    } else {
-        return (
-            <div className ="details-pane-container">
-                <h2>Right Text</h2>
-            </div>
-        )       
+      });
+    } catch (e) {
+        console.log(e)
     }
 }
 
