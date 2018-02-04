448709f3e66eb30f934bd470e0c8b6be3c107e94
Jake <jesmith24@comcast.net>
2018-02-03 23:35:15 -0800

    routing prepared for jack

--- yelp-help/src/components/DetailsPane.js
+++ yelp-help/src/components/DetailsPane.js
@@ -5,6 +5,20 @@ import request from 'request'
 
 const DetailsPane = (props) => {
     const my_id = props.match.params.place
+    // try {
+    //     var options = { method: 'GET',
+    //       url: 'http://localhost:8080/lookup/',
+    //       qs: {id : my_id}
+    //     };
+  
+    //   request(options, function (error, response, body) {
+    //     if (error) throw new Error(error);
+    //     var json = JSON.parse(body)
+    //     console.log(json)
+    //   });
+    // } catch (e) {
+    //     console.log(e)
+    // }
     try {
         var options = { method: 'GET',
           url: 'http://localhost:8080/lookup/',
@@ -13,8 +27,23 @@ const DetailsPane = (props) => {
   
       request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        var json = JSON.parse(body)
-        console.log(json)
+        //Begin request to Jack using raw body
+        try {
+            var options = { method: 'GET',
+              url: 'http://localhost:8181//',
+              qs: {reviews : body}
+            };
+      
+          request(options, function (error, response, body) {
+            if (error) throw new Error(error);
+            var jack_json = JSON.parse(body)
+            console.log(jack_json)
+            //end request to jack
+            
+          });
+        } catch (e) {
+            console.log(e)
+        }
       });
     } catch (e) {
         console.log(e)
