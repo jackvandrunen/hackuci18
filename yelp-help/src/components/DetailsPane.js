6a1d5512d166eec48f2bfa9df6a89fcd8b99a7a5
Jake <jesmith24@comcast.net>
2018-02-04 07:30:07 -0800

    one request instead of two

--- yelp-help/src/components/DetailsPane.js
+++ yelp-help/src/components/DetailsPane.js
@@ -5,20 +5,6 @@ import request from 'request'
 
 const DetailsPane = (props) => {
     const my_id = props.match.params.place
-    // try {
-    //     var options = { method: 'GET',
-    //       url: 'http://localhost:8080/lookup/',
-    //       qs: {id : my_id}
-    //     };
-  
-    //   request(options, function (error, response, body) {
-    //     if (error) throw new Error(error);
-    //     var json = JSON.parse(body)
-    //     console.log(json)
-    //   });
-    // } catch (e) {
-    //     console.log(e)
-    // }
     try {
         var options = { method: 'GET',
           url: 'http://localhost:8080/lookup/',
@@ -27,28 +13,42 @@ const DetailsPane = (props) => {
   
       request(options, function (error, response, body) {
         if (error) throw new Error(error);
-        //Begin request to Jack using raw body
-        try {
-            var options = { method: 'GET',
-              url: 'http://localhost:8181/menu/',
-              qs: {reviews : body},
-              mode: 'no-cors'
-            };
-      
-          request(options, function (error, response, body) {
-            if (error) throw new Error(error);
-            var jack_json = JSON.parse(body)
-            console.log(jack_json)
-            //end request to jack
-            
-          });
-        } catch (e) {
-            console.log(e)
-        }
+        var json = JSON.parse(body)
+        console.log(json)
       });
     } catch (e) {
         console.log(e)
     }
+    // try {
+    //     var options = { method: 'GET',
+    //       url: 'http://localhost:8080/lookup/',
+    //       qs: {id : my_id}
+    //     };
+  
+    //   request(options, function (error, response, body) {
+    //     if (error) throw new Error(error);
+    //     //Begin request to Jack using raw body
+    //     try {
+    //         var options = { method: 'GET',
+    //           url: 'http://localhost:8181/menu/',
+    //           qs: {reviews : body},
+    //           mode: 'no-cors'
+    //         };
+      
+    //       request(options, function (error, response, body) {
+    //         if (error) throw new Error(error);
+    //         var jack_json = JSON.parse(body)
+    //         console.log(jack_json)
+    //         //end request to jack
+            
+    //       });
+    //     } catch (e) {
+    //         console.log(e)
+    //     }
+    //   });
+    // } catch (e) {
+    //     console.log(e)
+    // }
 
     if (props.match.params.place !== '') {
         return (
