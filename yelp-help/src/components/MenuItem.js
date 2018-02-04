1272091596e6023b90b6e9f2e4aadf028a052162
Jake <jesmith24@comcast.net>
2018-02-04 08:08:14 -0800

    new files

---
+++ yelp-help/src/components/MenuItem.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './MenuItem.css'
+
+class MenuItem extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render () {
+        const item = this.props.info.item
+        const rating = this.props.info.rating
+        const containerClasses = 'menu-list-item-container'
+        return (
+            <div className={containerClasses}>
+                <div className="menu-list-item">
+                    <div className="menu-list-item-description">
+                        <h2>{item}</h2>
+                        <p>{rating}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default ListItem
